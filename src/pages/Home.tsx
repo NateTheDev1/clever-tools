75faeff0225c4c8a78583053942c693398f84870
Nathaniel Richards <nathaniel@datacompusa.com>
2021-07-28 02:15:35 -0400

    Working on dashboard

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,29 @@ const Home = () => {
 		<div className="page-container">
 			<Navbar />
 			<div className="home p-8">
-				<h1>Home</h1>
+				<h3 className="header-title">DASHBOARD</h3>
+				<hr />
+				<div className="row">
+					<div className="stat p-8">
+						<h4>Total Properties</h4>
+						<p>41</p>
+					</div>
+					<div className="stat p-8">
+						<h4>Total Open Rooms</h4>
+						<p>13</p>
+					</div>
+				</div>
+				<div className="row">
+					<div className="stat p-8">
+						<h4>Total Rooms</h4>
+						<p>335</p>
+					</div>
+					<div className="stat p-8">
+						<h4>Total Unavailable Rooms</h4>
+						<p>130</p>
+					</div>
+				</div>
+				<hr />
 			</div>
 		</div>
 	);
